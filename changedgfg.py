baea75aaffe6e8604368a3725c1780fe9c44fd36
gitactionsdivya <divyasivasankaran1998@gmail.com>
2023-09-05 13:45:33 +0530

    first gitaction program

---
+++ changedgfg.py
@@ -0,0 +1,12 @@
+"""
+This is a simple program that demonstrates variable initialization and addition.
+"""
+
+# Initialize constant 'A' with the value 1
+A = 1
+
+# Initialize constant 'B' with the value 2
+B = 2
+
+# Print the sum of 'A' and 'B'
+print(A + B)  # Output: 3
